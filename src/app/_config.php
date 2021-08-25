ed75271d088438d1a3ffe3f63c658399b207bc94
Kevin Gröger <k.groeger@headtrip.eu>
2021-08-25 11:44:11 +0200

    initial

---
+++ src/app/_config.php
@@ -0,0 +1,15 @@
+<?php
+
+use SilverStripe\Security\DefaultAdminService;
+use SilverStripe\Security\PasswordValidator;
+use SilverStripe\Security\Member;
+
+$username = 'test';
+$password = 'test';
+
+// remove PasswordValidator for SilverStripe 5.0
+$validator = PasswordValidator::create();
+// Settings are registered via Injector configuration - see passwords.yml in framework
+Member::set_password_validator($validator);
+
+DefaultAdminService::setDefaultAdmin($username, $password);
